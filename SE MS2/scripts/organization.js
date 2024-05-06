d96d008333ec8db3fc5ca3f071d63c41a0cc5e6f
mostafaelsewedy <mostafa03ashraf@gmail.com>
2024-05-06 23:15:43 +0300

    a few tweaks here and there mainly notifications and edits in post

--- SE MS2/scripts/organization.js
+++ SE MS2/scripts/organization.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const postButton = document.querySelector(".post-button");
     const postNow = document.getElementById("postnow");
     const xbutt = document.getElementById("closepop");
+    const notifications = document.querySelectorAll(".notification");
+    const volunteers = document.querySelectorAll(".volunteer");
 
     buttonsRows.forEach(buttonRow => {
         buttonRow.addEventListener("click", function() {
@@ -11,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
             switch (buttonText) {
                 case "home":
-                    window.location.href = "hompage.html";
+                    document.getElementById("postsarea").style.display = "block";
                     break;
                 case "documents":
                     document.getElementById("postsarea").style.display = "none";
@@ -48,8 +50,22 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("postpopup").style.display = "none";
     });
 
-    //volunteer when clicked on info
+    notifications.forEach(notification => {
+        viewButton = notification.querySelector(".notification button");
+        viewButton.addEventListener("click",function(){
+            //show more details regarding notification
+        });
+        xbutton = notification.querySelector(".notification img");
+        xbutton.addEventListener("click",function(){
+            notification.style.display = "none";
+        });
+    });
+
+    volunteers.forEach(volunteer => {
+        volunteer.addEventListener("click",function(){
+            //show info in pop up same location as notifications
+        });
+    });
 
     //org when clicked on
-
 });
